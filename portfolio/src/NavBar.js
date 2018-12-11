34ec1a1775946ba7ab1ca51255b9d2f9ccd413c4
katiemacaulay <katiemmacaulay@gmail.com>
2018-12-11 17:07:30 -0600

    updated things

--- portfolio/src/NavBar.js
+++ portfolio/src/NavBar.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import webLogo from './webDesign.jpeg'
 
 class App extends Component {
 
     render() {
       return (
-        <div class="flex justify-center bb" >
-            <Button variant="outlined ba b--white mr3">Home</Button>
-            <Button variant="outlined ba b--white mr2">About</Button>
-            <h1 class="mr4 ml4">Katie Macaulay</h1>
-            <Button variant="outlined ba b--white mr2">Work</Button>
-            <Button variant="outlined ba b--white mr2">Contact</Button>
+        <div>
+          <div class="flex justify-center bb" >
+              <Button variant="outlined ba b--white mr3">Home</Button>
+              <Button variant="outlined ba b--white mr2">About</Button>
+              <div>
+                <h1 class="mr4 ml4">Katie Macaulay</h1>
+                {/* <img src={webLogo} class= "mt0 ml5 h3"/> */}
+              </div>
+              <Button variant="outlined ba b--white mr2">Work</Button>
+              <Button variant="outlined ba b--white mr2">Contact</Button>
+          </div>
+          <img src={webLogo} class="Negative-margin ml8 ml2 h3"/>
         </div>
       );
     }
--- portfolio/src/WorkLinks.js
+++ portfolio/src/WorkLinks.js
@@ -1,32 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
+import StackGrid from "react-stack-grid";
 
 class App extends Component {
-    render() {
-      return (
-        <GridList class="mdc-grid-list">
-          <GridListTileBar class="mdc-grid-list__tiles">
-            <GridListTile class="mdc-grid-tile">
-              <div>
-                <img class="mdc-grid-tile__primary-content" src="my-image.jpg" />
-              </div>
-              <span class="mdc-grid-tile__secondary">
-                <span class="mdc-grid-tile__title">Title</span>
-              </span>
-            </li>
-            <li class="mdc-grid-tile">
-              <div class="mdc-grid-tile__primary">
-                <img class="mdc-grid-tile__primary-content" src="my-image.jpg" />
-              </div>
-              <span class="mdc-grid-tile__secondary">
-                <span class="mdc-grid-tile__title">Title</span>
-              </span>
-            </li>
-          </ul>
-        </div>
+  render() {
+    return (
+      <StackGrid
+        columnWidth={150}
+      >
+        <div key="key1">Item 1</div>
+        <div key="key2">Item 2</div>
+        <div key="key3">Item 3</div>
+      </StackGrid>
       );
     }
   }
